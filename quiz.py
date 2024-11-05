c96b3f7805c637575bda824f9b5ca6e7f2994f73
donsa <donca.p.r@gmail.com>
2024-11-05 14:16:41 -0300

    x

--- quiz.py
+++ quiz.py
@@ -7,12 +7,23 @@ st.set_page_config(
     page_icon="https://cultlight.com.br/wp-content/uploads/logo-padrao-150x150.png"
 )
 
+st.markdown("""
+    <style>
+    [role=radiogroup]{
+        gap: 3rem;
+    }
+    </style>
+    """,unsafe_allow_html=True)
+
 def main():
 
     # Render image from URL in Streamlit
     st.image("https://cultlight.com.br/wp-content/uploads/logo-cultlight-horizontal.png", width=200)
 
     st.subheader("Quiz da Cultlight")
+    st.write("Teste seus conhecimentos canábicos!")
+
+    st.write("---")
 
     # Initialize session state
     if 'history' not in st.session_state:
@@ -26,10 +37,10 @@ def main():
         return node
 
     current_node = get_current_node(quiz, st.session_state.history)
-    st.write(current_node['question'])
+    st.subheader(f"**{current_node['question']}**")
 
     if current_node['options']:
-        selected_option = st.radio("Choose an option:", current_node['options'], key=len(st.session_state.history))
+        selected_option = st.radio("", current_node['options'], key=len(st.session_state.history))
         if st.button("Next"):
             st.session_state.history.append(selected_option)
             st.rerun()
